e6a8228bd2f9aec7ad2f60b456ab6c0a00f1ef3e
Vlad <vlad.ios.develop@gmail.com>
2022-08-08 14:41:01 +0300

    Created UI element

--- Instagram/Instagram/Controllers/Other/Onboarding/LoginViewController.swift
+++ Instagram/Instagram/Controllers/Other/Onboarding/LoginViewController.swift
@@ -7,12 +7,84 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+final class LoginViewController: UIViewController {
 
+//MARK: - Private constants
+    private let userNameEmailField: UITextField = {
+        let field = UITextField()
+        return field
+    }()
+    
+    private let passwordField: UITextField = {
+        let field = UITextField()
+        field.isSecureTextEntry = true
+        return field
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    private let createAcountButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    private let termsButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    private let privacyButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    private let headerView: UIView = {
+        let header = UIView()
+        return header
+    }()
+    
+    
+    //MARK: - View lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .red
+        addSubviews()
+        view.backgroundColor = .systemBackground
     }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+    }
+
+}
+
+//MARK: - Private extension
+private extension LoginViewController {
+
+    func addSubviews() {
+        view.addSubview(userNameEmailField)
+        view.addSubview(passwordField)
+        view.addSubview(loginButton)
+        view.addSubview(termsButton)
+        view.addSubview(privacyButton)
+        view.addSubview(headerView)
+        view.addSubview(createAcountButton)
+    }
+}
+
+//MARK: - @Objc private extension
+@objc private extension LoginViewController {
+    
+    func didTapLoginButton() {}
+    
+    func didTapTermsButton() {}
+    
+    func didTapPrivacyButton() {}
+    
+    func didTapCreateAcountButtin() {}
 
 }
--- Instagram/Instagram/Controllers/Other/Onboarding/RegistrationViewController.swift
+++ Instagram/Instagram/Controllers/Other/Onboarding/RegistrationViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class RegistrationViewController: UIViewController {
+final class RegistrationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
